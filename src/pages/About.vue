222078be0a59b52c7a85df88d9a4b821032a1428
f1anderz <ssttaass0415@gmail.com>
2021-11-02 21:02:32 +0200

    about us added

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,20 +1,42 @@
 <template>
-  <div>
-    <h1 style="text-align: center;"><span style="font-size: x-large;">About Us !</span></h1>
-    <h3 style="text-align: center;"><span style="font-weight: normal;"><span style="font-size: large;">Hello Friends Welcome To STEPcommerce</span></span></h3>
+  <div class="main">
+    <div>STEPcommerce is an online store that has been delighting its customers with exceptionally high-quality products for many years. Here you can purchase
+      everything you need for home, rest, work and the like. We strive to provide you with the best in store, with a focus on reliability and quality.
+      We hope we are helpful to you.</div>
 
-      STEPcommerce is a Professional Shop Platform. Here we will provide you only interesting content, which you will like very much.
+    <div>Our competent employees work closely with our customers, creating a solid foundation for our innovative developments.
+      Working in a friendly family atmosphere allows us to recognize and meet the needs of our clients in a timely manner.</div>
 
-          We're dedicated to providing you the best of Shop, with a focus on dependability and Technologies. Our About-us page was created with the help of the <a href="https://techbuddies.in/">About-us Generator</a>
-            We're working to turn our passion for Shop into a booming online website. We hope you enjoy our Shop as much as we enjoy offering them to you.
-          <div style="text-align: center;"></div>
-          <div style="text-align: center;">
-            <h3>I will keep posting more important posts on my Website for all of you. Please give your support and love.</h3>
-          </div>
-          <div style="text-align: center;">Thanks For Visiting Our Site</div>
-          <div style="text-align: center;"><a href="http://localhost:8080/faq" target="_blank">
-              <span style="font-size: medium;">Contact Us !</span></a>
-          </div>
+    <div>High quality of the company and customer satisfaction is our top priority in
+      business. Therefore, we continue to improve product quality and have
+      an innovative approach in everything. Striving for technological innovation in our
+      sector, we aim to become a leading brand of innovative products.</div>
+
+    <div>European standards. High quality</div>
+
+    <div>Fundamentals of quality policy:
+      We are constantly improving and developing the quality of our products in order to make
+      a new perspective on our sector.</div>
+
+    <div>We strive to satisfy our customers by fulfilling their expectations for quality and
+      the cost of goods and related services at a certain time.</div>
+
+    <div>We guarantee that all our employees will work with great dedication and
+      professional experience within the framework of the Quality Management System.</div>
+
+    <div>We strongly believe that the achievements and successes of our employees play
+      a very important role in the expansion of our company.</div>
+
+    <div>In this regard, we regularly train our employees to identify
+      optimal customer orientation based on risk assessment preventive
+      approach, improving the professional quality of services and constant updating
+      submission of information.</div>
+
+    <div>We work in accordance with the requirements of the Quality Management System,
+      customer and regulatory requirements.</div>
+
+    <div>We strive to continuously improve our Quality Management System and
+      competitiveness with the participation of all employees of the company.</div>
   </div>
 </template>
 
@@ -25,5 +47,14 @@ export default {
 </script>
 
 <style scoped>
-
+.main{
+  width: 76%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 110px;
+  font-size: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
 </style>
